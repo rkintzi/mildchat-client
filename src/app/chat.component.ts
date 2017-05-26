cd8458c24763b15a8ef98f22869d0fcc27db170c
Łukasz Wojciechowski <nostrzak@gmail.com>
2017-05-26 21:16:14 +0200

    Scroll to bottom on new message.

--- src/app/chat.component.ts
+++ src/app/chat.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit ,Component, ViewChild, ElementRef } from '@angular/core';
 import { ChatService } from './chat.service';
 
 @Component({
     selector: 'chat',
     template: `
-        <div class="messages">
+        <div class="messages" #messagesElem>
             <p *ngFor="let msg of messages; let last = last">{{ msg }}</p>
         </div>
     `,
+    styles: [
+        `:host { overflow-y: scroll; flex: 1; }`,
+    ]
 })
-export class Chat {
+export class Chat implements AfterViewInit{
+    @ViewChild('messagesElem') messagesElem: ElementRef;
     messages: string[] = new Array();
 
-    constructor(private chatService: ChatService) {
+    constructor(
+        private chatService: ChatService,
+        private elRef: ElementRef,
+    ) {
         chatService.messages.subscribe(msg => {
             this.messages.push(msg.toString());
         });
+    }
+
+    ngAfterViewInit() {
+        let observer = new MutationObserver(() => {
+            this.elRef.nativeElement.scrollTop = this.messagesElem.nativeElement.offsetHeight;
+        });
+        observer.observe(this.messagesElem.nativeElement, { childList: true });
     }
 }
