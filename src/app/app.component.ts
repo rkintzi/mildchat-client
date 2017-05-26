5d8fe8e5e73bf2f395f27485b0902a769cce50ac
Radosław Kintzi <r.kintzi@pobox.net.pl>
2017-05-26 11:25:12 +0200

    Strip to stage1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 
-import {ChatService} from './chat.service'
 
 @Component({
     selector: 'app',
     template: `
-        <div class="wrapper">
-            <navbar></navbar>
-            <chat></chat>
-            <create-message></create-message>
+        <div>
+        Hello World
         </div>
     `,
-    providers: [ChatService],
 })
 
 export class AppComponent {}
